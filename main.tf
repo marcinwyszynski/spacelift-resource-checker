2d1376c46058dae3266397c04f997d9dc0ef15b4
Marcin Wyszynski <marcin.pixie@gmail.com>
2023-06-13 14:13:04 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "spacelift" {}
+
+resource "spacelift_stack" "validator" {
+  name        = "Spacelift resource validator"
+  description = "Continuous validation for Spacelift resources"
+  
+  administrative = true
+  project_root   = "validator"
+  after_plan     = ["terraform show -json spacelift.plan | jq -c '.prior_state.values.root_module.resources[0].values' > check.custom.spacelift.json"]
+
+  raw_git {
+     namespace = "marcinwyszynski"
+     url = "https://github.com/marcinwyszynski/spacelift-resource-checker.git"
+  }
+  
+  repository = "spacelift-resource-checker"
+}
